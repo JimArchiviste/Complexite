8d0dfc1aa4771828ed8f24994a847f3c2cfe9b87
UltimateCookie <loic.vierin@gmail.com>
2015-10-26 20:49:33 +0100

    test maximal

--- extract.c
+++ extract.c
@@ -4,23 +4,30 @@
 graph exMaximal(graph g)
 {
 	graph gmax;
-	exMaximal_rec(g, 0, 0, &gmax);
+	exMaximal_rec(g, 0, 0, &gmax, &g);
+	return gmax;
 }
 
-void exMaximal_rec(graph g, int i, int j, graph* gmax)
+void exMaximal_rec(graph g, int i, int j, graph* gmax, graph* g)
 {
-	if( i != j )
+	if( ! Maximal(gmax, g) ) //si le sous graphe n'est pas maximal
 	{
-		if( g.edges[i][j] != 1 ) 
+		if( verification(gmax, g) ) //si le sous graphe est bien un sous graphe desert
 		{
-			//if(verif(gmax,j)) //verification dans gmax
-			gmax->edges[i][j] = 1;
-			gmax->edges[j][i] = 1;
+				gmax->edges[i][j] = 1;
+				gmax->edges[j][i] = 1;
+				exMaximal_incr(&i,&j,g); //on avance
+				exMaximal_rec(g,i,j,gmax,g);
+			}
+		else {
+				exMaximal_decr(&i,&j,g);
+				gmax->edges[i][j] = 0;
+				gmax->edges[j][i] = 0;
 		}
 	}
-		
-		
-	exMaximal_incr(&i, &j, g);	
+	//sinon il est maximal
+			
+	
 }
 				
 		
@@ -29,14 +36,31 @@ void exMaximal_incr(int* i, int* j, graph g)
 {
 	if (*j == g.n )
 	{
-		
-		*i = *i+1;
+		if( *i != g.n )
+			*i = *i+1;
 		*j = 0;
 	}
 	
 	else
 	{
 		*j=*j+1;
+	}
+	
+}
+
+
+void exMaximal_decr(int* i, int* j, graph g)
+{
+	if (*j == 0 )
+	{
+		if(*i!= 0)
+			*i = *i-1;
+		*j = g.n;
+	}
+	
+	else
+	{
+		*j=*j-1;
 	}
 	
 }
