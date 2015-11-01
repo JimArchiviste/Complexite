481cc48ccdd66411c22adad4f651f49117db1341
laiaga <leonardialexandre@free.fr>
2015-11-01 15:18:02 +0100

    desert max incomplet

---
+++ maximum_incomplet.h
@@ -0,0 +1,9 @@
+#ifndef DESERT_MAXIMUM_INCOMPLET
+#define DESERT_MAXIMUM_INCOMPLET
+
+int max_arcs (int tailleGraphe, int graphe[tailleGraphe][tailleGraphe], int sousGraphe[tailleGraphe]);
+int[] maximum_incomplet(int tailleGraphe, int graphe[tailleGraphe][tailleGraphe]);
+
+#endif
+
+
